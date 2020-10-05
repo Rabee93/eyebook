231d6badf767fbd39432fa93ba9b6f95ae979f84
Rabee Mghrabi <learningprogramming2019@gmail.com>
2020-10-05 21:56:31 +0100

    added edit, update and delete functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,44 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    if params.has_key? "let"
+      unless current_user
+        redirect_to new_user_session_path
+      else
+        @let = true
+        @posts = current_user.posts
+      end
+    else
+      @let = false
+      @posts = Post.all
+    end
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+      @post = Post.find(params[:id])
+
+      @post.update(post_params)
+      redirect_to posts_path("let" => true)
+    end
+
+    def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+
+      redirect_to posts_path("let" => true)
+    end
+
+
 def new
-  unless current_user
-    redirect_to new_user_session_path
-  end
+  @post = Post.new
   end
   def create
     current_user.posts.create(post_params)
+    redirect_to posts_path("let" => true)
   end
 
   private
